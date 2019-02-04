55ec2d5eddccbb7f4e3e08e24373bc5267bbfaf0
Jakub Kubasiak <xjk@pfa.dk>
2019-02-04 09:21:53 +0100

    upgrade ui

--- TTS/View/MainWindow.xaml.cs
+++ TTS/View/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MahApps.Metro.Controls;
 using TTS.ViewModel;
 
 namespace TTS
@@ -19,7 +20,7 @@ namespace TTS
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : MetroWindow
     {
         public MainWindow()
         {
