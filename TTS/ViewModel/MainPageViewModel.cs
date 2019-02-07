fb9e2fd9f5c79e8a5b4abc99969037931ef73d31
Jakub Kubasiak <xjk@pfa.dk>
2019-02-07 11:14:09 +0100

    clean solution

--- TTS/ViewModel/MainPageViewModel.cs
+++ TTS/ViewModel/MainPageViewModel.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.IO;
 using System.Linq;
@@ -8,11 +7,9 @@ using System.Runtime.CompilerServices;
 using System.Speech.Recognition;
 using System.Speech.Synthesis;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
 using Microsoft.Win32;
-using NAudio.Wave;
 using TTS.Annotations;
 using TTS.Command;
 
